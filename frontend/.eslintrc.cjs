3039b84be2e94a51b7afaf4252b6bd12d20aad42
Dmitry Zakharchenko <zah_dm@hotmail.com>
2024-06-13 22:20:47 +0300

    2nd part completed - ui design

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -12,10 +12,36 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "semi": "off",
+    "no-debugger": "warn",
+    "no-underscore-dangle": 0,
+    "max-len": [
+      2,
+      {
+        "code": 160,
+        "ignoreUrls": true
+      }
     ],
+    "react/prop-types": 0,
+    "react/state-in-constructor": 0,
+    "react/static-property-placement": 0,
+    "import/no-extraneous-dependencies": 0,
+    "react/no-deprecated": 0,
+    "react/destructuring-assignment": 0,
+    "no-useless-escape": 0,
+    "import/prefer-default-export": 0,
+    "react/function-component-definition": 0,
+    "no-unsafe-optional-chaining": 0,
+    "function-paren-newline": 0,
+    "default-param-last": 0,
+    "react/jsx-no-useless-fragment": 0,
+    "react/no-unstable-nested-components": 0,
+    "react/jsx-props-no-spreading": 0,
+    "react/jsx-uses-vars": "error",
+    "react/jsx-uses-react": "error",
+    "object-curly-spacing": [
+      "error",
+      "always"
+    ]
   },
 }
---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,31 @@
+import Conversation from './Conversation.jsx';
+
+const Conversations = () => {
+    return (
+        <div className="py-2 flex flex-col overflow-auto">
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+        </div>
+    )
+}
+
+export default Conversations;
+
+
+// STARTER CODE FOR THIS FILE
+//import Conversation from './Conversation.jsx';
+//
+//const Conversations = () => {
+//    return (
+//        <div className="py-2 flex flex-col overflow-auto">
+//            <Conversation />
+//            <Conversation />
+//            <Conversation />
+//            <Conversation />
+//        </div>
+//    )
+//}
+//
+//export default Conversations;
